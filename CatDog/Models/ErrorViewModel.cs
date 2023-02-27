e037ac05ea53e756cea633e13e5b532096e50bb4
bread133 <76678991+bread133@users.noreply.github.com>
2023-02-27 12:35:00 +0300

    Add project files.

---
+++ CatDog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CatDog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
